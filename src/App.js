ad249074022d41631d227386b6885ed22a97adb7
Sachintha Sampath <sachinsampath42@gmail.com>
2024-08-30 23:32:18 +0530

    feat: useTimeOut

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UseLocalStorage from './components/UseLocalStorage';
+import UseTimeOut from './components/UseTimeOut';
 import UseToggle from './components/UseToggle';
 
 function App() {
   return (
     <div className="App">
       {/* <UseLocalStorage /> */}
-      <UseToggle />
+      {/* <UseToggle /> */}
+      <UseTimeOut/>
     </div>
   );
 }
---
+++ src/components/UseTimeOut.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import useTimeOut from '../hooks/useTimeOut';
+
+const UseTimeOut = () => {
+  const [count, setCount] = useState(0);
+
+  const { clear, reset } = useTimeOut(() => setCount(0), 2000);
+
+  return (
+    <div>
+      <p>{count.toString()}</p>
+      <div>
+        <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+        <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+      <div>
+        <button onClick={clear}>Clear Timeout</button>
+        <button onClick={reset}>Reset Timeout</button>
+      </div>
+    </div>
+  );
+};
+
+export default UseTimeOut;
